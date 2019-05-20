ab39c49eff64371732aa4d181be3c57837df7170
Guilherme Nunes <guilhermenunes09@protonmail.com>
2019-05-20 09:33:03 +0000

    Send multiple prayers

--- app/mailers/reply_request_mailer.rb
+++ app/mailers/reply_request_mailer.rb
@@ -1,8 +1,8 @@
 class ReplyRequestMailer < ApplicationMailer
     def reply_request(request)
         @request = request
-        @category = request.categories.first
-        @prayer = @category.prayers.first.prayer
+        @categories = request.categories
+        
         mail(to: @request.email, subject: "Re: Comunidade Bahá'í do Brasil")
     end
 end
