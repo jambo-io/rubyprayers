cbb5b51dd19d43d7beca24867d597f116fc10e01
guilhermenunes09 <guilhermenunes09@protonmail.com>
2019-05-23 09:46:27 +0000

    Email checking html sanitize

--- app/jobs/gmail_job.rb
+++ app/jobs/gmail_job.rb
@@ -6,22 +6,30 @@ class GmailJob < ApplicationJob
     require 'gmail'
     gmail = Gmail.new(Rails.application.credentials.gmail[:imap_user], Rails.application.credentials.gmail[:imap_password])
 
-    gmail.mailbox('rubyprayers').emails(:unstarred, :after => Date.parse("2018-08-31"), :from =>  "no-reply@parastorage.com").each do |email|
+    gmail.mailbox('rubyprayers').emails(:unstarred, :after => Date.parse("2018-08-31"), :from =>  "oracoesonline@bahai.org.br").each do |email|
 
         # => Header
         head = email.header.to_s
         head_encoded = head.force_encoding('utf-8')
         datetime = head_encoded.split("Date:").last.split("From:").first
 
-        #no_tags = Sanitize.clean(email.html_part.body.decoded)
         # => Body
         body = email.html_part.body.decoded
-        body_encoded = body.force_encoding('utf-8')
+        # Eliminate HMTL tags
+        body_encoded = Sanitize.clean(body.force_encoding('utf-8'))
 
-        name = body_encoded.split("<b>Nome</b>").last.split("</li>").first.strip!
-        user_email = body_encoded.split("<b>Email</b>").last.split("</li>").first.strip!
-        message = body_encoded.split("<b>Pedido de oração</b>").last.split("</li>").first.strip!
-        phone = body.split("<b>Telefone</b>").last.split("</li>").first.strip!
+        name = body_encoded.split("Nome:").last.split("Email:").first.strip!
+
+        puts "Name: #{name}"
+
+        user_email = body_encoded.split("Email:").last.split("Telefone:").first.strip!
+        puts "Email: #{user_email}"
+
+        phone = body.split("Telefone:").last.split("Pedido de oração:").first.strip!
+        puts "Phone: #{phone}"
+
+        message = body_encoded.split("Pedido de oração:").last.split("Para").first.strip!
+        puts "Message: #{message}"
 
         request = Request.new
         request.name = name
