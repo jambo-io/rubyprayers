95b5fba09706a74d4631fc961957af560a85b1dd
guilhermenunes09 <guilhermenunes09@protonmail.com>
2019-05-23 09:52:57 +0000

    Email job

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,17 +108,4 @@ Rails.application.configure do
   #   authentication:       'plain',
   #   enable_starttls_auto: true  }
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'guilhermewnunes@gmail.com'}
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'example.com',
-  user_name:            'guilhermewnunes@gmail.com',
-  password:             'biH2SryfurT4Tng',
-  authentication:       'plain',
-  enable_starttls_auto: true  }
 end
